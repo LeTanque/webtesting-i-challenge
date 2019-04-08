960e8e71ca74b27b00e445f60047b9182ef8b373
Frank Martinez <frankie.martinez@gmail.com>
2019-04-08 15:42:42 -0700

    initial test structures complete. Working on client requirements

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,48 @@
 module.exports = {
+  repair,
   succeed,
   fail,
-  repair,
-  get,
+  // get,
 };
 
-function succeed(item) {
-  return { ...item };
+function succeed(item, enhancement) {
+  if (!item) item = 'sword'
+  if (!enhancement) enhancement=12
+  item = {
+    item:item,
+    durability:100,
+    enhancement:enhancement
+  }
+  if (item.enhancement > 10 && item.enhancement < 21) return { ...item };
+  return false;
 }
 
-function fail(item) {
-  return { ...item };
+function fail(item, enhancement) {
+  if (!item) item = 'sword'
+  if (!enhancement) enhancement=9
+  item = {
+    item:item,
+    durability:100,
+    enhancement:enhancement
+  }
+  if (item.enhancement >= 0 && item.enhancement < 11) return { ...item };
+  return false;
 }
+
 
 function repair(item) {
+  if (!item) item = 'sword'
+  item = {
+    item:item,
+    durability:100,
+    enhancement:0
+  }
   return { ...item };
 }
 
-function get(item) {
-  return { ...item };
-}
+// function get(item) {
+//   return { ...item };
+// }
+
+// console.log(repair('sword'))
+console.log(fail('sword'))
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,23 @@
-const enhancer = require('./enhancer.js');
+const {
+    repair,
+    succeed,
+    fail,
+} = require('./enhancer.js');
+
 // test away!
+describe('enhancer()', () => {
+    it('repair() item durability should be 100', () => {
+        expect(repair('sword').durability).toBe(100)
+    })
+
+    it('succeed(item) enhancement that succeeds', () => {
+        expect(succeed('sword').enhancement).toBeGreaterThan(10)
+    });
+
+    it('fail(item) enhancement that fails', () => {
+        expect(fail('sword').enhancement).toBeLessThan(11)
+    });
+
+
+    it.todo('get()');
+});
